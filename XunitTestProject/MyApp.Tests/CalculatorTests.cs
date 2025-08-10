fc31dfa8d4c8580573a189bc1d81d8f15d76e61f
Rajdip27 <srajdip920@gmail.com>
2025-08-10 16:41:31 +0600

    Xunit Test Project Create 😊🦴

---
+++ XunitTestProject/MyApp.Tests/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿namespace XunitTestProject.MyApp.Tests;
+
+public class CalculatorTests
+{
+    [Theory]
+    [InlineData(2, 3, 5)]
+    [InlineData(-1, -1, -2)]
+    [InlineData(0, 5, 5)]
+    public void Add_ShouldReturnCorrectSum(int a, int b, int expected)
+    {
+        var calc = new Calculator();
+        var result = calc.Add(a, b);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ XunitTestProject/MyApp/Calculator.cs
@@ -0,0 +1,6 @@
+﻿namespace XunitTestProject.MyApp;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+}
